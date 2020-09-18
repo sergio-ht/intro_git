1cd844e2768281993d8083930908c80d47d86b6d
Sergio Hinojosa <sergio.hinojosa12945@alumnos.udg.mx>
2020-09-18 17:07:55 -0500

    Se imprime si el numero es par o impar

--- test.py
+++ test.py
@@ -5,3 +5,8 @@ print("Bienvenido, {}!".format(name))
 
 num = int(input("Numero: "))
 print("{}! = {}".format(num,math.factorial(num)))
+
+if(num % 2 == 0):
+    print(num,"es par")
+else:
+    print(num,"es impar")
